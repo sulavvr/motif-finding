e1c4c7a5ad546f659f8c9b25cb73f4c32da640dc
Sulav Regmi <sulavvr@gmail.com>
2017-02-26 16:41:21 -0600

    Allow fasta file input

--- main.py
+++ main.py
@@ -2,83 +2,88 @@
 # Advanced Analysis of Algorithms
 # Project: Motif finding using KMP Algorithm
 # Members:
-# 	Hitesh Arora (50489713),
-# 	Sulav Regmi (50211843)
+#   Hitesh Arora (50489713),
+#   Sulav Regmi (50211843)
 # Dr. Huang
 # ##############################################
+#
 
-string_o = 'atgaccgggatactgatagaagaaaggttgggggcgtacacattagataaacgtatgaagtacgttagactcggcgccgccgacccctattttttgagcagatttagtgacctggaaaaaaaatttgagtacaaaacttttccgaatacaataaaacggcgggatgagtatccctgggatgacttaaaataatggagtggtgctctcccgatttttgaatatgtaggatcattcgccagggtccga' # - i
 
-
-string_t = 'gctgagaattggatgcaaaaaaagggattgtccacgcaatcgcgaaccaacgcggacccaaaggcaagaccgataaaggagatcccttttgcggtaatgtgccgggaggctggttacgtagggaagccctaacggacttaatataataaaggaagggcttataggtcaatcatgttcttgtgaatggatttaacaataagggctgggaccgcttggcgcacccaaattcagtgtgggcgagcgcaa'
-string_th = 'cggttttggcccttgttagaggcccccgtataaacaaggagggccaattatgagagagctaatctatcgcgtgcgtgttcataacttgagttaaaaaatagggagccctggggcacatacaagaggagtcttccttatcagttaatgctgtatgacactatgtattggcccattggctaaaagcccaacttgacaaatggaagatagaatccttgcatactaaaaaggagcggaccgaaagggaag'
+def readFromFile():
+    sequences = []
+    line_idx = 0
+    with open('sequence.fasta') as f:
+        content = f.read()
+        for line in content.splitlines():
+            if line_idx % 2 == 0:
+                line_idx += 1
+                continue
+            else:
+                line_idx += 1
+                sequences.append(line)
+    return sequences
 
 
 def check(lookup_arr, result, pattern, string):
-	i, j = 0, 0
-	while j < len(lookup_arr):
-		if i < len(string):
-			if string[i] == pattern[j]:
-				result += string[i]
-				i += 1
-				j += 1
-			else:
-				if j != 0:
-					j -= 1
-					j = lookup_arr[j]
-					continue
-				else:
-					i += 1
-					continue
-		else:
-			break
-	return result[-len(pattern):]# == pattern
+    i, j = 0, 0
+    while j < len(lookup_arr):
+        if i < len(string):
+            if string[i] == pattern[j]:
+                result += string[i]
+                i += 1
+                j += 1
+            else:
+                if j != 0:
+                    j -= 1
+                    j = lookup_arr[j]
+                    continue
+                else:
+                    i += 1
+                    continue
+        else:
+            break
+    return result[-len(pattern):]
+
 
 def setupLookup(pattern):
-	i, j = 1, 0
-	arr = [0] * len(pattern)
+    i, j = 1, 0
+    arr = [0] * len(pattern)
 
-	while i < len(pattern):
-		if pattern[j] == pattern[i]:
-			arr[i] = j + 1
-			i += 1
-			j += 1
-		else:
-			if j != 0:
-				j -= 1
-				j = arr[j]
-				continue
-			else:
-				arr[i] = 0
-				i += 1
-				continue
-	return arr
+    while i < len(pattern):
+        if pattern[j] == pattern[i]:
+            arr[i] = j + 1
+            i += 1
+            j += 1
+        else:
+            if j != 0:
+                j -= 1
+                j = arr[j]
+                continue
+            else:
+                arr[i] = 0
+                i += 1
+                continue
+    return arr
+
+sequences = readFromFile()
+
 
 idx = 0
-while idx < len(string_o) - 15:
-	ptrn = string_o[idx:(idx+15)]
+while idx < len(sequences[0]) - 15:
+    ptrn = sequences[0][idx:(idx+15)]
 
-	res = ''
-	arr = setupLookup(ptrn)
+    res = ''
+    arr = setupLookup(ptrn)
 
-	matched_t = check(arr, res, ptrn, string_t)
-	matched_th = check(arr, res, ptrn, string_th)
+    matched = []
+    for sequence in range(1, len(sequences)):
+        matched.append(check(arr, res, ptrn, sequences[sequence]))
 
-	if matched_t == ptrn and matched_th == ptrn:
-		print(ptrn, ' matched in both sequence.')
-		break
-	else:
-		idx += 1
-		continue
-
-# print(idx)
-
-
-
-
-# res = ''
-# arr = setupLookup()
-# matched = check(arr, res)
-
-# print(matched)
-
+    if (all(x == ptrn for x in matched)):
+        print(ptrn, ' matched in all sequences.')
+        break
+    else:
+        idx += 1
+        if idx == len(sequences[0]) - 16:
+            print('Match not found in all sequences!')
+        continue
