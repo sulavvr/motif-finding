d0cdb719baeb6b0447c8013b207180ac65b2f652
Hitesh Arora <hitesh-arora@live.com>
2017-02-26 17:48:49 -0600

    Comments added

--- main.py
+++ main.py
@@ -5,11 +5,19 @@
 #   Hitesh Arora (50489713),
 #   Sulav Regmi (50211843)
 # Dr. Huang
-# ##############################################
 #
+# Using KMP algorithm or Knuth-Morris-Pratt string searching algorithm
+# to find Motif in the sequences
+# ##############################################
+
 
 
 def readFromFile():
+
+    """
+    Read sequences from sequence.fasta file and store each sequence in a list by skipping every other line
+     :return: list of sequences
+    """
     sequences = []
     line_idx = 0
     with open('sequence.fasta') as f:
@@ -25,6 +33,15 @@ def readFromFile():
 
 
 def check(lookup_arr, result, pattern, string):
+
+    """
+    Compares provided pattern against the string passed, part of the KMP algorithm
+    :param lookup_arr: list of
+    :param result: empty string for returning
+    :param pattern: pattern to be matched against the sequence
+    :param string: sequence to be matched against
+    :return: string of size of pattern length
+    """
     i, j = 0, 0
     while j < len(lookup_arr):
         if i < len(string):
@@ -46,6 +63,12 @@ def check(lookup_arr, result, pattern, string):
 
 
 def setupLookup(pattern):
+
+    """
+    It build us the prefix list, which is part of the KMP algorithm
+    :param pattern: pattern to be matched against the sequence
+    :return: prefix list
+    """
     i, j = 1, 0
     arr = [0] * len(pattern)
 
@@ -65,12 +88,18 @@ def setupLookup(pattern):
                 continue
     return arr
 
+
+PATTERN_LENGTH = 15
+
+# storing list of all the sequences return by the function
 sequences = readFromFile()
 
+# looping through the sequence taking string of length same as pattern length
+# and then using KMP to find match in all sequences
 
 idx = 0
-while idx < len(sequences[0]) - 15:
-    ptrn = sequences[0][idx:(idx+15)]
+while idx < len(sequences[0]) - PATTERN_LENGTH:
+    ptrn = sequences[0][idx:(idx + PATTERN_LENGTH)]
 
     res = ''
     arr = setupLookup(ptrn)
